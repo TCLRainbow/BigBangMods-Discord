925107e12d9af31d4dbddb25559224860f8b6f76
Pythonic-Rainbow <wongchingho0@gmail.com>
2021-08-05 22:31:01 +0800

    1.0 for Pandora

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 BigBangMods-Discord
+
+
+**PLEASE NOTE THAT** you have to manually remove `com` package inside the shadowed JAR when you build the project.
---
+++ src/main/java/chingdim/bbmcord/BBMcord.java
@@ -0,0 +1,28 @@
+package chingdim.bbmcord;
+
+import net.arikia.dev.drpc.DiscordEventHandlers;
+import net.arikia.dev.drpc.DiscordRPC;
+import net.arikia.dev.drpc.DiscordRichPresence;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("bbmcord")
+public class BBMcord {
+
+    public BBMcord() {
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    }
+
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        DiscordRPC.discordInitialize("869624084311445564", new DiscordEventHandlers(), true);
+        DiscordRichPresence rpc = new DiscordRichPresence.Builder("Pandora")
+                .setStartTimestamps(System.currentTimeMillis() / 1000)
+                .build();
+        DiscordRPC.discordUpdatePresence(rpc);
+    }
+}
